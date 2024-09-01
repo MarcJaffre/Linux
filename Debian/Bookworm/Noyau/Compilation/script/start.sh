416a4c1849705e6adee29db48f7bedf36c7741e4
Marc <teste74@hotmail.fr>
2024-09-01 18:04:37 +0200

    Update start.sh

--- Debian/Bookworm/Noyau/Compilation/script/start.sh
+++ Debian/Bookworm/Noyau/Compilation/script/start.sh
@@ -62,6 +62,6 @@ if [ -d $DOSSIER/linux-${VERSION} ]; then
    echo "-----------------------------------------------------------------------------" >> $DOSSIER/linux-${VERSION}/Build.log;
    echo "Demarrage du script : $START_DATE à $START_HORAIRE";                           >> $DOSSIER/linux-${VERSION}/Build.log;
    echo "Fn du script        : $END_DATE à $END_HORAIRE";                               >> $DOSSIER/linux-${VERSION}/Build.log;
-   mv $                        DOSSIER/linux-${VERSION}/Build.log                          $DOSSIER/linux-${VERSION}/Build_$START_DATE_$START_HORAIRE.log;
+   mv                          $DOSSIER/linux-${VERSION}/Build.log                         $DOSSIER/linux-${VERSION}/Build_$START_DATE_$START_HORAIRE.log;
    #########################################################################################################################################################
 fi
