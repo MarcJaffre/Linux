2d059453aac3827e9013372f1cb64383eec3c225
Marc <teste74@hotmail.fr>
2024-09-01 20:27:52 +0200

    Update general.sh

--- Debian/Bookworm/Noyau/Compilation/script/general.sh
+++ Debian/Bookworm/Noyau/Compilation/script/general.sh
@@ -8,13 +8,26 @@ source settings;
 ######################################################################################################################################################
 # Fonctions #
 #############
-KERNEL_PURGE(){}
+KERNEL_PURGE(){
+ cd $DOSSIER;
+ rm -rf $DOSSIER/linux-${VERSION}* 2>/dev/null;
+}
 
-DOWNLOAD_KERNEL(){}
+DOWNLOAD_KERNEL(){
+ cd $DOSSIER;
+ wget $KERNEL $DOSSIER/linux-${VERSION}.tar.xz             2>/dev/null;
+ tar xf       $DOSSIER/linux-${VERSION}.tar.xz -C $DOSSIER 1>/dev/null;
+}
 
-KERNEL_EDIT(){}
+KERNEL_EDIT(){
+ cd $DOSSIER/linux-${VERSION};
+ make xconfig;
+}
 
-KERNEL_CLEAN(){}
+KERNEL_CLEAN(){
+ cd $DOSSIER/linux-${VERSION};
+ make clean;
+}
 
 KERNEL_COMPILATION(){}
 
